ae439fcbb4c8837fd5535fcb423a1a6ba2f00dc4
dvoichuck <dvoichuck@student.ucode.world>
2021-03-07 22:25:28 +0200

    view holders working

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-let width = document.getElementById('img').style.width
-console.log(width)
+let img_src = {
+    1: ["project",
+        "./assets/images/artistic-brush.png",
+        "./assets/images/artistic-brush-active.png",
+    ],
+    2: ['aboutMe',
+        "./assets/images/user-icon.png",
+        "./assets/images/user-icon-active.png",
+    ],
+    3: ['contacts',
+        "./assets/images/sms-speech-bubble.png",
+        "./assets/images/sms-speech-bubble-active.png",
+    ],
+    4: ['userInfo',
+        "./assets/images/computer-screen.png",
+        "./assets/images/computer-screen-active.png",
+    ]
+}
+let imgOff = -1
+function call(key) {
+    if (imgOff !== -1)
+        document.getElementById(`${img_src[imgOff][0]}`).src = img_src[imgOff][1]
+    document.getElementById('setting').innerHTML = ''
+    document.getElementById('setting').insertAdjacentHTML('afterbegin', img_src[key][0])
+    document.getElementById(`${img_src[key][0]}`).src = img_src[key][2]
+    imgOff = key
+}
